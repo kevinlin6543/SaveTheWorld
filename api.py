c11bd4407afe476b30a13352efeba8a68603f50f
Yingzhi <1995hyz@gmail.com>
2018-10-13 00:24:39 -0400

    Merge

--- api.py
+++ api.py
@@ -26,6 +26,28 @@ def get_recipe_steps(req):
         print(str(j[i]['number']) + ": " + j[i]['step'])
         i += 1
     # print(json_data['instructions'])
+	
+
+def search_recipt_by_ingredients(ingredients, fill_ingredients=False, number=10, ranking=1):
+    ingredients_str = ''
+    ingredients_str += '%2C'.join(x for x in ingredients)
+    print(ingredients_str)
+    if fill_ingredients:
+        fill_ingredients_str = 'true'
+    else:
+        fill_ingredients_str = 'false'
+    send_msg = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients="+\
+               fill_ingredients_str+"&ingredients="+ingredients_str+"&limitLicense=false&number="+str(number)+\
+               "&ranking="+str(ranking)
+    return send_msg
+
+
+def title_to_id(response_msg):
+    response_list = json.loads(response_msg.text)
+    response_dic = {}
+    for x in response_list:
+        response_dic[x['title']] = x['id']
+    return response_dic
 
 
 r = requests.get(get_recipe(479101),
